4ddf9f061510ff27e13d8a10c714466ed4c34780
yulia-poluhina-d8 <56877064+yulia-poluhina-d8@users.noreply.github.com>
2024-06-22 18:25:02 +0300

    updates

--- project/scripts/index.js
+++ project/scripts/index.js
@@ -5,9 +5,9 @@ function bannerSwitcher() {
   let isInput = next.classList.contains("sec-1-input");
 
   if (isInput && next != null && next != undefined) {
-    let checkedValue = (document.querySelector(
+    checkedValue = document.querySelector(
       ".sec-1-input:checked"
-    ).checked = false);
+    ).checked = false;
     next.checked = true;
   } else {
     let first = document.querySelectorAll(".sec-1-input")[0];
